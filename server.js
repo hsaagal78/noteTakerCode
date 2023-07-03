5d3cbef81f2697cedbd9e205f7127a9b84d70d85
hsagal78 <hsagal78@gmail.com>
2023-07-03 12:44:31 -0400

    fixed index.html. now save data

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const Note = require('./lib/Note');
+const Note = require('./lib/Note.js');
 const path = require ('path')
 
 const app = express();
 
 app.use(express.static('./public/html'));
+app.use(express.urlencoded({ extended: false }));
 app.get('/public/css/style.css', (req, res) => {
     res.setHeader('Content-Type', 'text/css');
     res.sendFile(path.join(__dirname, 'public', 'css', 'style.css',));
@@ -13,7 +14,7 @@ app.get('/public/css/style.css', (req, res) => {
     res.setHeader('Content-Type', 'text/css');
     res.sendFile(path.join(__dirname, 'public', 'css', 'jass.css',));
   });
-app.use(express.urlencoded({ extended: false }));
+
 
 // Load Routes
 
@@ -21,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.post('/notes', (clientReq, serverRes) => {
 
   const newNote = new Note(clientReq.body.note);
-
+  console.log(clientReq.body);
   newNote.save();
 
   // Respond back to the client to complete the request
